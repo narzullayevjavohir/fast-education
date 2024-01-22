2dfdeffe8f2237517997a8a9f36d304e2e79040f
Samandar0021305 <tsamandardev89@gmail.com>
2024-01-22 22:33:45 +0500

    config

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,14 @@ import "./assets/scss/index.scss";
 
 import router from './router/index';
 
+import { useComponents } from "@/sam-design"
 
 const app = createApp(App)
 const pinia = createPinia();
+
+const { SIcon } = useComponents()
+
+app.component("s-icon",SIcon)
 
 app.use(router);
 app.use(pinia);
---
+++ src/sam-design/index.ts
@@ -0,0 +1,5 @@
+import SIcon from "./app/icon.vue";
+
+export const useComponents = () => {
+    return { SIcon }
+}
